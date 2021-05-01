25983adb2dee95d2409276e512fa8b454046e4e3
ahmet dalhançer <ahmed44dalhancer@hotmail.com>
2021-05-01 17:26:13 +0300

    Workshop 1

---
+++ intro/src/intro/main.java
@@ -0,0 +1,20 @@
+package intro;
+
+public class main {
+
+	public static void main(String[] args) {
+		String internetSubeButonu="Ýnternet Þubesi";
+		int benimYasim=27;
+		boolean yasimDogrulandiMi=false;
+		double cebimdeKacParaVar=205.20;
+		System.out.println(internetSubeButonu);
+		System.out.println("Yaþým"+" "+benimYasim+ "Yaþým Doðrulandý mý?"+":"+yasimDogrulandiMi);
+		
+		if(benimYasim==26) {
+			System.out.println("Doðum Gününüz Kutlu olsun");
+		}else {
+			System.out.println("Geber Amk");
+		}
+	}
+
+}
