3c01271447cf7b3cca58e9e5e493a9b381f5ae0a
ahmet dalhançer <ahmed44dalhancer@hotmail.com>
2021-05-02 15:44:53 +0300

    Commit 3

--- static/src/Validation.java
+++ static/src/Validation.java
@@ -1,5 +1,8 @@
 
 public class Validation {
+	static {
+		System.out.println("construtor çalýþtý");
+	}
 	public static boolean isValid(Product product) {
 		if(!product.name.isEmpty()) {
 			return true;
