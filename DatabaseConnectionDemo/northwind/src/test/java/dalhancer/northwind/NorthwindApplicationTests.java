e1f1a18700876c52c119940ce97b6dcc7d2e5bab
ahmet dalhançer <ahmed44dalhancer@hotmail.com>
2021-05-09 14:47:31 +0300

    Hibernate, spring bot kullanarak veritabanından veri çekme. Commit 1

---
+++ DatabaseConnectionDemo/northwind/src/test/java/dalhancer/northwind/NorthwindApplicationTests.java
@@ -0,0 +1,13 @@
+package dalhancer.northwind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
