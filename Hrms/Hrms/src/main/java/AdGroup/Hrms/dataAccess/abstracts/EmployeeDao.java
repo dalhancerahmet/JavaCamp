beb712b7d3d72968abf358333f75ba135850efe8
Ahmet Dalhançer <76035836+dalhancerahmet@users.noreply.github.com>
2021-05-10 15:03:26 +0300

    Delete Hrms/Hrms directory

--- Hrms/Hrms/src/main/java/AdGroup/Hrms/dataAccess/abstracts/EmployeeDao.java
+++
@@ -1,9 +0,0 @@
-package AdGroup.Hrms.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import AdGroup.Hrms.entities.concretes.Employee;
-
-public interface EmployeeDao extends JpaRepository<Employee, Integer> {
-
-}
